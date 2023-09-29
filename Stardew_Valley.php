88c4378bf864fd652d2ad5cd7d18e16a1da85f74
SantiagoDiVirgilio <135436329+SantiagoDiVirgilio@users.noreply.github.com>
2023-09-29 00:10:54 -0300

    Stardew Valley Completo

--- Stardew_Valley.php
+++ Stardew_Valley.php
@@ -31,7 +31,7 @@
                     <li><a href="secundarios.php">BBBBBBBBBBB</a></li>
                     <li><a href="cameos.php">CCCCCCCCCCCCC</a></li>
                 </div>
-            </ul>
+            </ul> 
         </div>
         <div class="dropdown">
             <ul>
@@ -45,120 +45,56 @@
         </div>
 </header>
 
-<section class="generos" id="accion">
-    <h2>ACCIÓN</h2>
-    <div>
-        <a href="Cuphead.php"><img src="images/Capturas/Cuphead/Cuphead_Portada.jpg" alt="Portada de Cuphead"></a>
-        <h3>CUPHEAD</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-</section>
 
-<section class="generos" id="indie">
-    <h2>CASUAL</h2>
-    <div>
-        <a href="Stardew_Valley.php"><img src="images/Capturas/Stardew_Valley/Stardew_Valley_Portada.jpg" alt="Portada de Stardew Valley"></a>
-        <h3>STARDEW VALLEY</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-</section>
+<h2 class = "titulos">STARDEW VALLEY</h2>
 
-<section class="generos" id="terror">
-    <h2>TERROR</h2>
-        <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
+<article class="juegos">
     <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-</section>
-
-<section class="generos" id="shooter">
-    <h2>SHOOTER</h2>
-
-    <div>
-        <a href="PayDay_2.php"><img src="images/Capturas/PayDay_2/PayDay_Portada.jpg" alt="Portada de Pay Day 2"></a>
-        <h3>PAY DAY 2</h3>
+       <iframe width="800" height="500" src="https://www.youtube.com/embed/81zM_vg96VY" title="Cuphead | Tráiler de lanzamiento" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 
     <div>
-        <a href="Counter_Old.php"><img src="images/Capturas/Counter_Old/Counter_Old_Portada.jpg" href="Counter_Old.php" alt="Portada de Counter Strike 1.6"></a>
-        <h3>COUNTER STRIKE 1.6</h3>
+        <img src="images/Capturas/Stardew_Valley/Stardew_Valley_Portada.jpg" alt="Portada de Stardew Valley">
+        <p>Acabas de heredar la vieja parcela agrícola de tu abuelo de Stardew Valley. Decides partir hacia una nueva vida con unas herramientas usadas y algunas monedas. ¿Te ves capaz de vivir de la tierra y convertir estos campos descuidados en un hogar próspero?.</p>
+        <br>
+        <h4>FECHA DE LANZAMIENTO: 26 FEB 2016
+        <br>
+        <br>
+        DESARROLLADOR: <a href="https://www.stardewvalley.net/" target="_blank">ConcernedApe</a> 
+        <br>
+        EDITOR: <a href="https://www.stardewvalley.net/" target="_blank">ConcernedApe</a>  
+        </h4>
     </div>
-    
-    <div>
-        <a href="Team_Fortress_2.php"><img src="images/Capturas/Team_Fortress_2/Team_Fortress_2_Portada.jpg" alt="Portada de Team Fortress 2"></a>
-        <h3>TEAM FORTRESS 2</h3>
-    </div>
-</section>
 
-<section class="generos" id="roguelike">
-    <h2>ROGUELIKE</h2>
-        <div>
-        <img src="images/Capturas/The_Binding_Of_Isaac/The_Binding_Of_Isaac_Portada.jpg" alt="">
-        <h3>THE BINDING OF ISAAC: REBIRTH</h3>
-    </div>
-    <div>
-        <img src="images/Capturas/Voidigo/Voidigo_Portada.jpg" alt="">
-        <h3>VOIDIGO</h3>
-    </div>
-    <div>
-        <img src="images/Capturas/Dead_Cells/Dead_Cells_Portada.jpg" alt="">
-        <h3>DEAD CELLS</h3>
-    </div>
-</section>
+</article>
 
-<section class="generos" id="accion">
-    <h2>AVENTURA</h2>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-</section>
+<div class = "reseña">
+<h3>DEJA TU RESEÑA</h3>
+<textarea name="" id="" cols="30" rows="10"></textarea>
+</div>
 
-<section class="generos" id="accion">
-    <h2>RPG</h2>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-    <div>
-        <img src="images/Vapor_Logo_White.png" alt="">
-        <h3>AAAAAA</h3>
-    </div>
-</section>
+<div class= "acerca_de">
+<h3>ACERCA DE ESTE JUEGO</h3>
+<br>
+<br>
+<p>Te vas a vivir al valle...
+<br>Acabas de heredar la vieja parcela agrícola de tu abuelo de Stardew Valley. Decides partir hacia una nueva vida con unas herramientas usadas y algunas monedas. ¿Te ves capaz de vivir de la tierra y convertir estos campos descuidados en un hogar próspero?.
+<br><br><strong>¡Crea la granja de tus sueños!</strong> Constrúyela desde cero en una de las cinco configuraciones del mapa.
+<br><br><strong>¡Domina tu habilidad con la ganadería!</strong> Cría animales, siembra cultivos y fabrica maquinaria útil entre muchas más cosas.
+<br><br><strong>¡Únete a la comunidad local!</strong> Entabla amistad con más de 30 habitantes de Pelican Town.
+<br><br><strong>¡Personaliza a tu granjero!</strong> Podrás elegir entre cientos de opciones de personalización de personajes.
+<br><br><strong>¡Instálate y empieza a formar una familia!</strong> Comparte tu vida en la granja con uno de los doce personajes con los que podrás tener una relación.
+<br><br><strong>¡Explora grandes y misteriosas cuevas!</strong> Encuentra monstruos peligrosos y tesoros valiosos.</p>
+</div>
+
+
+<div class= "acerca_de">
+<h3>REQUISITOS DEL JUEGO</h3>
+<br>
+<br>
+<p>MÍNIMO: SO: Windows Vista or greater, Procesador: 2 Ghz, Memoria: 2 GB de RAM, Gráficos: 256 mb video memory, shader model 3.0+, DirectX: Versión 10, Almacenamiento: 500 MB de espacio disponible.</p>
+</div>
+
 
 <footer>
     <div>
