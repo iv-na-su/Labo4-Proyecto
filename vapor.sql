f72fbd1a19bbdd7d1d26f705ed9a08b5ef9c95e8
SantiagoDiVirgilio <135436329+SantiagoDiVirgilio@users.noreply.github.com>
2023-10-19 19:05:37 -0300

    DALE BOQUITAAAAAAA

--- vapor.sql
+++ vapor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2023 a las 21:51:05
+-- Tiempo de generación: 20-10-2023 a las 00:00:15
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
