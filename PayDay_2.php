8e988eedd46991961d47981f59d1f1cbf6ba405c
Ivan Nahuel Suarez <naahuel.suarez.2000@gmail.com>
2023-10-25 09:24:57 -0300

    FOOTER

--- PayDay_2.php
+++ PayDay_2.php
@@ -119,31 +119,9 @@ Compatibilidad con RV: SteamVR. Standing or Room Scale</p>
 
 
 <footer>
-    <div>
-        <a href="index.php"><strong>Inicio</strong></a>
-    </div>
-    <div>
-        <a href="juegos.php"><strong>Juegos</strong></a>
-            <br><a href="#accion">Acción</a>
-            <br><a href="#casual">Casual</a>
-            <br><a href="#terror">Terror</a>
-            <br><a href="#shooter">Shooter</a>
-            <br><a href="#roguelike">Roguelike</a>
-            <br><a href="#aventura">Aventura</a>
-            <br><a href="#rpg">Rpg</a>
-    </div>
-    <div>
-        <h7><strong>Perfil</strong></h7>
-    </div>
-    <div>
-        <h7><strong>Contacto</strong></h7>
-    </div>
-    <br>
-    <a href="https://es-la.facebook.com/" target="_blank"><img src="images/Icono_Facebook.png" alt="Facebook"></a>
-    <a href="https://www.instagram.com/" target="_blank"><img src="images/Icono_Instagram.png" alt="Instagram"></a>
-    <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZXMifQ%3D%3D%22%7D" target="_blank"><img src="images/Icono_Twitter.png" alt="Twitter"></a>
-    <a href="https://www.tiktok.com/es/" target="_blank"><img src="images/Icono_TikTok.png" alt="TikTok"></a>
-    <a href="https://web.snapchat.com/" target="_blank"><img src="images/Icono_Snapchat.png" alt="Snapchat"></a>
+<?php
+    include("FOOTER.php");
+    ?>
 </footer>
 
 <script type="text/javascript">
--- juegos.php
+++ juegos.php
@@ -152,38 +152,9 @@ while ($cont <= 7) {
 </section>
 
 <footer>
-    <div>
-        <a href="index.php"><strong>Inicio</strong></a>
-    </div>
-    <div>
-        <a href="juegos.php"><strong>Juegos</strong></a>
-            <br><a href="#accion">Acción</a>
-            <br><a href="#casual">Casual</a>
-            <br><a href="#terror">Terror</a>
-            <br><a href="#shooter">Shooter</a>
-            <br><a href="#roguelike">Roguelike</a>
-            <br><a href="#aventura">Aventura</a>
-            <br><a href="#rpg">Rpg</a>
-    </div>
-    <div>
-        <h7><strong>Perfil</strong></h7>
-        <!--<br><a href="resumen.html">Resumen</a>
-        <br><a href="origen.html">Origen</a>
-        <br><a href="derivados.html">Derivados</a> -->
-    </div>
-    <div>
-        <h7><strong>Contacto</strong></h7>
-        <!--<br><a href="produccion.html">Producción</a>
-        <br><a href="impacto.html">Impacto Cultural</a>
-        <br><a href="temporadas.html">Temporadas</a>
-        <br><a href="prensa.html">Datos de Prensa</a> -->
-    </div>
-    <br>
-    <a href="https://es-la.facebook.com/" target="_blank"><img src="images/Icono_Facebook.png" alt="Facebook"></a>
-    <a href="https://www.instagram.com/" target="_blank"><img src="images/Icono_Instagram.png" alt="Instagram"></a>
-    <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZXMifQ%3D%3D%22%7D" target="_blank"><img src="images/Icono_Twitter.png" alt="Twitter"></a>
-    <a href="https://www.tiktok.com/es/" target="_blank"><img src="images/Icono_TikTok.png" alt="TikTok"></a>
-    <a href="https://web.snapchat.com/" target="_blank"><img src="images/Icono_Snapchat.png" alt="Snapchat"></a>
+<?php
+    include("FOOTER.php");
+    ?>
 </footer>
 
 <script type="text/javascript">
