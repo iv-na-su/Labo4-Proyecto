ef207ec5a270e24b55e537433d96da134d7ce78d
SantiagoDiVirgilio <135436329+SantiagoDiVirgilio@users.noreply.github.com>
2023-10-27 14:53:46 -0300

    .

--- FOOTER.php
+++ FOOTER.php
@@ -4,13 +4,6 @@
     </div>
     <div>
         <a href="juegos.php"><strong>Juegos</strong></a>
-            <br><a href="#accion">Acción</a>
-            <br><a href="#casual">Casual</a>
-            <br><a href="#terror">Terror</a>
-            <br><a href="#shooter">Shooter</a>
-            <br><a href="#roguelike">Roguelike</a>
-            <br><a href="#aventura">Aventura</a>
-            <br><a href="#rpg">Rpg</a>
     </div>
     <div>
         <h7><strong>Perfil</strong></h7>
@@ -22,6 +15,4 @@
     <a href="https://es-la.facebook.com/" target="_blank"><img src="images/Icono_Facebook.png" alt="Facebook"></a>
     <a href="https://www.instagram.com/" target="_blank"><img src="images/Icono_Instagram.png" alt="Instagram"></a>
     <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZXMifQ%3D%3D%22%7D" target="_blank"><img src="images/Icono_Twitter.png" alt="Twitter"></a>
-    <a href="https://www.tiktok.com/es/" target="_blank"><img src="images/Icono_TikTok.png" alt="TikTok"></a>
-    <a href="https://web.snapchat.com/" target="_blank"><img src="images/Icono_Snapchat.png" alt="Snapchat"></a>
 </html>
